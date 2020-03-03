c54a9396c5cae353d6519b140200e4b4f1d54008
Sender Rodrigues dos Santos <sender.rsantos@gmail.com>
2020-03-03 13:16:51 -0300

    Add project files.

---
+++ ExercicioFileDirectory/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ExercicioFileDirectory {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
