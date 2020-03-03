df17c138bca6130e080beb2f815564f8fabfa6a1
Sender Rodrigues dos Santos <sender.rsantos@gmail.com>
2020-03-03 17:17:50 -0300

    sem comentarios ainda

---
+++ ExercicioFileDirectory/Entidades/Produtos.cs
@@ -0,0 +1,22 @@
+﻿namespace ExercicioFileDirectory.Entidades {
+    class Produtos {
+        //propriedades autoimplementadas
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        //construtor padrão com argumentos.
+        public Produtos(string nome, double price, int quantidade) {
+            Nome = nome;
+            Preco = Preco;
+            Quantidade = quantidade;
+        }
+
+        //metodo do tipo double para calcular o total dos produtos
+        public double Total() {
+            return Preco * Quantidade;
+        }
+
+
+    }
+}
--- ExercicioFileDirectory/Program.cs
+++ ExercicioFileDirectory/Program.cs
@@ -1,9 +1,44 @@
 ﻿using System;
+using System.IO;
+using System.Globalization;
+using ExercicioFileDirectory.Entidades;
 
 namespace ExercicioFileDirectory {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            Console.Write("Digite o caminho inteiro do arquivo: ");
+            string caminhoOrigemArquivo = Console.ReadLine();
+
+            try {
+                string[] lines = File.ReadAllLines(caminhoOrigemArquivo);
+
+                string caminhoOrigemPasta = Path.GetDirectoryName(caminhoOrigemArquivo);
+                string caminhoDestinoPasta = caminhoOrigemArquivo + @"\out";
+                string caminhoDestinoArquivo = caminhoDestinoPasta + @"\summary.csv";
+
+                Directory.CreateDirectory(caminhoDestinoPasta);
+
+                using (StreamWriter sw = File.AppendText(caminhoDestinoArquivo)) {
+                    foreach (string line in lines) {
+
+                        string[] fields = line.Split(',');
+                        string nome = fields[0];
+                        double preco = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantidade = int.Parse(fields[2]);
+
+                        Produtos prod = new Produtos(nome, preco, quantidade);
+                        sw.WriteLine(prod.Nome + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+
+                }
+                
+
+            }
+            catch (IOException e) {
+                Console.WriteLine("An Error Ocurred" );
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
